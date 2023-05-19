fda31d1aa205d857abbea0b6fdd5efe48f70c5af
MarvinBotchway <marvinbotchway@outlook.com>
2023-05-19 00:24:54 +0000

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - prints exactly with proper grammar,
+  * but the outcome is a piece of art,, followed by a new line.
+  *
+  * Return: Always 0 if successful 1 otherwise.
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
