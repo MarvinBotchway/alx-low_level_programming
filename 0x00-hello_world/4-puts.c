5e98e07be1e88b0b50e0431d497d91f7191c4a8e
MarvinBotchway <marvinbotchway@outlook.com>
2023-05-19 00:14:51 +0000

    Add 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints exactly "Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 if successful
+ */
+
+int main(void)
+{
+	char textToPrint[56] = "\"Programming is like building a multilingual puzzle";
+	puts(textToPrint);
+
+	return (0);
+}
