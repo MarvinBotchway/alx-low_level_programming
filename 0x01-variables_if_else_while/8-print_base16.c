8d7bdc7023e0e4bbe283262b94debd651b853af4
MarvinBotchway <marvinbotchway@outlook.com>
2023-05-20 00:49:13 +0000

    Add 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print all the numbers of base 16 in lowercase, followed by a new line.
+ *
+ * Return: 0 if successful 1 otherwise
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+		putchar(num);
+	for (num = 97; num < 103; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
+
