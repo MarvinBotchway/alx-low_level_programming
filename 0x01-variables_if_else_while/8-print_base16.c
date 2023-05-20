d0800a3ed56362bbc065bd24238cc4a1bc05472a
MarvinBotchway <marvinbotchway@outlook.com>
2023-05-20 00:51:10 +0000

    Fix Betty Issue In 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Print all the numbers of base 16 in lowercase, followed by a new line.
+ * main - Print all the numbers of base 16 in lowercase,
+ * followed by a new line.
  *
  * Return: 0 if successful 1 otherwise
  */
