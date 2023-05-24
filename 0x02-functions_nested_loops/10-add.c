ea794f180a79f687df0b5436b58cbeca3e2794f0
MarvinBotchway <marvinbotchway@outlook.com>
2023-05-24 01:57:06 +0000

    Add 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Add two integers
+ * @num1: First integer to add
+ * @num2: Second integer to add
+ * Return: The Sum of num1 and num 2.
+ */
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+
+	return (sum);
+}
