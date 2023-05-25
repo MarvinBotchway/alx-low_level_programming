091d3f71b86cc4477831a04877ce87e7578075a5
MarvinBotchway <marvinbotchway@outlook.com>
2023-05-25 21:29:38 +0000

    Add 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * mul - Multiplies 2 ints
+  * @a: 1st int to multiply
+  * @b: 2nd int to multiply
+  *
+  * Return: The product of a and b
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
