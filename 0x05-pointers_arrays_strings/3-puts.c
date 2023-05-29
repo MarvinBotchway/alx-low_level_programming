edb323cea16609e68021d4186d1a391ba8789d82
MarvinBotchway <marvinbotchway@outlook.com>
2023-05-29 18:16:55 +0000

    Fix Betty Issues In 3-puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * -puts - Prints a string, followed by a new line, to stdout.
+ * _puts - Prints a string, followed by a new line, to stdout.
  * @str: The string to be printed
  * Return: Always void.
  */
 void _puts(char *str)
 {
 	int i;
-	
+
 	i = 0;
 	for (; *(str + i) != '\0'; i++)
-		_putchar(*(str + i));	
+		_putchar(*(str + i));
 }
