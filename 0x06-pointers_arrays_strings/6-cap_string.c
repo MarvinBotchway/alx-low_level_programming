497216f6a1a863332b44bf249e84bce540a155b4
MarvinBotchway <marvinbotchway@outlook.com>
2023-06-02 00:08:03 +0000

    Fix Capitalization Of First Character in 6-cap_sting.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,6 +13,8 @@ char *cap_string(char *s)
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
+		if ((*(s + i) >= 97 && s[i] <= 122 &&  i == 0))
+			s[i] = s[i] - 32;
 		for (j = 0; sep[j] != '\0'; j++)
 		{
 			if (*(s + i) >= 97 && s[i] <= 122 && s[i - 1] == sep[j])
