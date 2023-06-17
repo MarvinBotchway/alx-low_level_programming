97db9101ed7b38ff3471f5446fc08f85f2107f0e
MarvinBotchway <marvinbotchway@outlook.com>
2023-06-17 01:23:08 +0000

    Fix issue with code that is run if ptr != NULL in 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,34 +15,30 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *old_ptr;
 	unsigned int i, c;
 
-	old_ptr = (char *)ptr;
-
 	i = c = 0;
+	old_ptr = (char *)ptr;
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 	{
 		printf("Unable to allocate memory.");
 		return (NULL);
 	}
-
-	if (ptr != NULL)
+	if (new_size > old_size)
+		c = old_size;
+	else if (new_size < old_size)
+		c = new_size;
+	else if (new_size == old_size)
+		return (ptr);
+	for (i = 0; i < c; i++)
 	{
-		if (new_size > old_size)
-			c = old_size;
-		else if (new_size < old_size)
-			c = new_size;
-		else if (new_size == old_size)
-			return (ptr);
-		if (new_size == 0)
-		{
-			free(ptr);
-			return (NULL);
-		}
-		for (i = 0; i < c; i++)
-		{
-			new_ptr[i] = old_ptr[i];
-		}
+		new_ptr[i] = old_ptr[i];
 	}
+
 	free(ptr);
 
 	return (new_ptr);
