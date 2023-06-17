e4e84f1d259c07b2dd727e484498ee5213c55988
MarvinBotchway <marvinbotchway@outlook.com>
2023-06-17 01:00:27 +0000

    Fix issue with freeing prt even if NULL in 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -33,7 +33,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			c = new_size;
 		else if (new_size == old_size)
 			return (ptr);
-		if (old_size == 0)
+		if (new_size == 0)
 		{
 			free(ptr);
 			return (NULL);
@@ -42,8 +42,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		{
 			new_ptr[i] = old_ptr[i];
 		}
-		free(ptr);
 	}
+	free(ptr);
 
 	return (new_ptr);
 }
