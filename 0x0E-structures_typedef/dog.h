01d982f954bfca4ca46ec1748ef80659fbdfe556
MarvinBotchway <marvinbotchway@outlook.com>
2023-06-20 19:46:55 +0000

    Update dog.h with a new type dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef _DOG_H
 #define _DOG_H
-
 /**
  * struct dog - A new type struct dog
  * @name: The dogs name.
@@ -13,7 +12,7 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
