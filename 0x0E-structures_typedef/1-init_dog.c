a8d1262a35498062e4c24b5baa0e5302e32b82f0
MarvinBotchway <marvinbotchway@outlook.com>
2023-06-20 18:37:54 +0000

    Add 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - Initialize a variable of type struct dog.
+ * @d: The struct dog.
+ * @name: The dogs name.
+ * @age: The dogs age.
+ * @owner: The dogs owner.
+ * Return: (void).
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
