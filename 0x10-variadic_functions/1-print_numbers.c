ae5ac0234956c7a6502f4e9a201cb99931828931
MarvinBotchway <marvinbotchway@outlook.com>
2023-06-22 22:55:35 +0000

    Fix check for n in 1-print_number.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,21 +11,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
-	if (n)
+	va_start(args, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(args, n);
-		for (i = 0; i < n; i++)
-		{
-			if (separator)
-				printf("%d%s", va_arg(args, int), separator);
-			else
-				printf("%d", va_arg(args, int));
-
-			if (i + 2 == n)
-				break;
-		}
-		printf("%d\n", va_arg(args, int));
-		va_end(args);
+		printf("%d", va_arg(args, int));
+		if (separator)
+			printf("%s", separator);
+		if (i + 2 == n)
+			break;
 	}
+	printf("%d\n", va_arg(args, int));
+	va_end(args);
 
 }
