81134d35b92d62156f6bf13c270723f9c90c8084
MarvinBotchway <marvinbotchway@outlook.com>
2023-06-23 17:58:09 +0000

    Add 3-print_all.c and print_all.h

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,42 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "print_all.h"
+/**
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function
+ *
+ * Return : (void)
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+
+	int i = 0;
+
+	format_t fts[] = {
+		{'c', "%c"},
+		{'i', "%i"},
+		{'f', "%f"},
+		{'s', "%s"},
+		{'\0', ""}
+	};
+
+	va_start(args, format);
+
+	while (fts[i].c != '\0' || format[i] == fts[i].c)
+	{
+		if (fts[i].c == 'c' || fts[i].c == 'i')
+			printf(fts[i].str, va_arg(args, int));
+		if (fts[i].c == 'f')
+			printf(fts[i].str, va_arg(args, double));
+		if (fts[i].c == 's')
+			printf(fts[i].str, va_arg(args, char *));
+		if (fts[i + 1].c != '\0')
+			printf(", ");
+
+		i++;
+	}
+	va_end(args);
+
+}
---
+++ 0x10-variadic_functions/print_all.h
@@ -0,0 +1,12 @@
+#ifndef PRINT_ALL_H
+#define PRINT_ALL_H
+/**
+ * struct op - Struct op
+ */
+typedef struct format_s
+{
+	char c;
+	char *str;
+
+} format_t;
+#endif /* PRINT_ALL_H */
